4d588de0a8200224bd89967b94e33d81e9a94ac6
Robin Kamboj <dev.robinkamboj@gmail.com>
2016-08-17 19:06:39 +0530

    95% static

--- app/src/main/java/swasthyalehar/swasthya/ActivitiesFragment.java
+++ app/src/main/java/swasthyalehar/swasthya/ActivitiesFragment.java
@@ -3,9 +3,14 @@ package swasthyalehar.swasthya;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -13,6 +18,9 @@ import android.view.ViewGroup;
  */
 public class ActivitiesFragment extends Fragment {
 
+
+    private RecyclerView recyclerview;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     public ActivitiesFragment() {
         // Required empty public constructor
@@ -23,7 +31,40 @@ public class ActivitiesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_activities, container, false);
+        View v = inflater.inflate(R.layout.fragment_activities, container, false);
+        List<Data> data = fill_with_data();
+
+        recyclerview = (RecyclerView) v.findViewById(R.id.recyclerview);
+        recyclerview.setHasFixedSize(true);
+
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        recyclerview.setLayoutManager(mLayoutManager);
+        Recycler_View_Adapter adapter = new Recycler_View_Adapter(data, getActivity().getApplication());
+        recyclerview.setAdapter(adapter);
+
+        return v;
     }
 
+    public List<Data> fill_with_data() {
+
+        List<Data> data = new ArrayList<>();
+        data.add(new Data("Activity_1", "Description", R.drawable.dp));
+
+        data.add(new Data("Activity_2", "Description", R.drawable.dp));
+
+        data.add(new Data("Activity_3", "Description", R.drawable.dp));
+
+        data.add(new Data("Activity_4", "Description", R.drawable.dp));
+
+        data.add(new Data("Activity_5", "Description", R.drawable.dp));
+
+        data.add(new Data("Activity_6", "Description", R.drawable.dp));
+
+        data.add(new Data("Activity_7", "Description", R.drawable.dp));
+
+        data.add(new Data("Activity_8", "Description", R.drawable.dp));
+
+        data.add(new Data("Activity_9", "Description", R.drawable.dp));
+        return data;
+    }
 }
--- app/src/main/java/swasthyalehar/swasthya/ProgressFragment.java
+++ app/src/main/java/swasthyalehar/swasthya/ProgressFragment.java
@@ -3,9 +3,14 @@ package swasthyalehar.swasthya;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -13,6 +18,8 @@ import android.view.ViewGroup;
  */
 public class ProgressFragment extends Fragment {
 
+    private RecyclerView recyclerview;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     public ProgressFragment() {
         // Required empty public constructor
@@ -23,7 +30,40 @@ public class ProgressFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_progress, container, false);
+        View v = inflater.inflate(R.layout.fragment_progress, container, false);
+        List<Data> data = fill_with_data();
+
+        recyclerview = (RecyclerView) v.findViewById(R.id.recyclerview);
+        recyclerview.setHasFixedSize(true);
+
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        recyclerview.setLayoutManager(mLayoutManager);
+        Recycler_View_Adapter adapter = new Recycler_View_Adapter(data, getActivity().getApplication());
+        recyclerview.setAdapter(adapter);
+
+        return v;
     }
 
+    public List<Data> fill_with_data() {
+
+        List<Data> data = new ArrayList<>();
+        data.add(new Data("Progress_1", "Description", R.drawable.dp));
+
+        data.add(new Data("Progress_2", "Description", R.drawable.dp));
+
+        data.add(new Data("Progress_3", "Description", R.drawable.dp));
+
+        data.add(new Data("Progress_4", "Description", R.drawable.dp));
+
+        data.add(new Data("Progress_5", "Description", R.drawable.dp));
+
+        data.add(new Data("Progress_6", "Description", R.drawable.dp));
+
+        data.add(new Data("Progress_7", "Description", R.drawable.dp));
+
+        data.add(new Data("Progress_8", "Description", R.drawable.dp));
+
+        data.add(new Data("Progress_9", "Description", R.drawable.dp));
+        return data;
+    }
 }
